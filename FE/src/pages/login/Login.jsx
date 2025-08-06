861894c4890838be60d609965fb2804660acb13c
bhyerain <seunghye0307@naver.com>
2025-08-06 16:51:38 +0900

    [Feat] 로그인 4개 페이지 구현

--- FE/src/pages/login/Login.jsx
+++ FE/src/pages/login/Login.jsx
@@ -4,94 +4,6 @@ import "../../styles/TextStyle.css";
 import google from "../../assets/google.png";
 import { useNavigate } from "react-router-dom";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  gap: 167px;
-  padding-top: 87px;
-`;
-const HomeLogo = styled.img`
-  width: 650px;
-  height: 650px;
-`;
-
-// 주황색 박스
-const LoginBox = styled.div`
-  border-radius: 100px;
-  width: 765px;
-  height: 906px;
-  background-color: rgba(255, 160, 122, 0.3);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 35px;
-`;
-
-// 아이디, 비밀번호 받는 텍스트 박스
-const TextBox = styled.input`
-  width: 515px;
-  height: 59px;
-  border-radius: 30px;
-  background-color: rgba(255, 255, 255, 0.7);
-  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-  border-width: 0;
-  font-size: 20px;
-  padding-left: 15px;
-`;
-
-// 텍스트 + 텍스트박스
-const InfoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 7px;
-`;
-
-const LoginButton = styled.button`
-  border-radius: 30px;
-  background-color: #ffbda2;
-  padding: 10px;
-  width: 150;
-  display: flex;
-  justify-content: center;
-  color: #2f2f2f;
-  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-  width: 154px;
-  border-width: 0;
-  cursor: pointer;
-`;
-
-const Line = styled.div`
-  background-color: #000000;
-  width: 130px;
-  height: 0;
-  border-top: 1px solid #000000;
-`;
-
-// 구글 로그인 버튼
-const GoogleButton = styled.button`
-  border-radius: 5px;
-  background-color: #ffffff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 60px;
-  width: 350px;
-  gap: 23px;
-  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-  border-width: 0;
-  cursor: pointer;
-`;
-
-const JoinButton = styled.button`
-  all: unset;
-  color: #ff6148;
-  cursor: pointer;
-  border-bottom: 2px solid #ff6148;
-`;
-
 export const Login = () => {
   const navigate = useNavigate();
   {
@@ -117,7 +29,7 @@ const [password, setPassword] = useState(null);*/
               style={{
                 textAlign: "center",
                 letterSpacing: 2,
-                paddingBottom: 35,
+                lineHeight: "150%",
               }}
             >
               집게로 꼭 쥐고 있던 링크,
@@ -130,13 +42,13 @@ const [password, setPassword] = useState(null);*/
             </InfoWrapper>
             <InfoWrapper className="body2">
               <div style={{ paddingLeft: 10 }}>비밀번호</div>
-              <TextBox />
+              <TextBox type="password" style={{ fontSize: 30 }} />
             </InfoWrapper>
             <LoginButton
-              className="heading3"
+              className="heading4"
               onClick={() => navigate("/loginok")}
             >
-              Login
+              로그인
             </LoginButton>
             <div
               style={{
@@ -179,3 +91,91 @@ const [password, setPassword] = useState(null);*/
     </>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 167px;
+  padding-top: 87px;
+`;
+const HomeLogo = styled.img`
+  width: 650px;
+  height: 650px;
+`;
+
+// 주황색 박스
+const LoginBox = styled.div`
+  border-radius: 100px;
+  width: 700px;
+  height: 800px;
+  background-color: rgba(255, 160, 122, 0.3);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 35px;
+`;
+
+// 아이디, 비밀번호 받는 텍스트 박스
+const TextBox = styled.input`
+  width: 515px;
+  height: 59px;
+  border-radius: 30px;
+  background-color: rgba(255, 255, 255, 0.7);
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  border-width: 0;
+  font-size: 20px;
+  padding-left: 15px;
+`;
+
+// 텍스트 + 텍스트박스
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+`;
+
+const LoginButton = styled.button`
+  border-radius: 30px;
+  background-color: #ffbda2;
+  padding: 10px;
+  width: 150;
+  display: flex;
+  justify-content: center;
+  color: #ffffff;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  width: 154px;
+  border-width: 0;
+  cursor: pointer;
+`;
+
+const Line = styled.div`
+  background-color: #000000;
+  width: 130px;
+  height: 0;
+  border-top: 1px solid #000000;
+`;
+
+// 구글 로그인 버튼
+const GoogleButton = styled.button`
+  border-radius: 5px;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  width: 350px;
+  gap: 23px;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  border-width: 0;
+  cursor: pointer;
+`;
+
+const JoinButton = styled.button`
+  all: unset;
+  color: #ff6148;
+  cursor: pointer;
+  border-bottom: 2px solid #ff6148;
+`;
--- FE/src/pages/login/SignUp.jsx
+++ FE/src/pages/login/SignUp.jsx
@@ -2,6 +2,92 @@ import logo from "../../assets/logo.png";
 import styled from "styled-components";
 import "../../styles/TextStyle.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export const SignUp = () => {
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+  const [isMismatch, setIsMismatch] = useState(false);
+
+  const handleSubmit = () => {
+    const mismatch = password !== checkPassword;
+    setIsMismatch(mismatch);
+
+    if (!mismatch) {
+      navigate("/setnickname");
+    }
+  };
+  return (
+    <>
+      <Container>
+        <HomeLogo src={logo} alt="linkrap image" />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <LoginBox>
+            <div
+              className="heading2"
+              style={{
+                textAlign: "center",
+                letterSpacing: 2,
+              }}
+            >
+              회원가입
+            </div>
+            <InfoWrapper className="body2">
+              <div style={{ paddingLeft: 10 }}>이메일</div>
+              <TextBox />
+            </InfoWrapper>
+            <InfoWrapper className="body2">
+              <div style={{ paddingLeft: 10 }}>아이디</div>
+              <TextBox />
+            </InfoWrapper>
+            <InfoWrapper className="body2">
+              <div style={{ paddingLeft: 10 }}>비밀번호</div>
+              <TextBox
+                type="password"
+                style={{ fontSize: 35 }}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </InfoWrapper>
+            <InfoWrapper className="body2">
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <div style={{ paddingLeft: 10 }}>비밀번호 확인</div>
+                {isMismatch && (
+                  <div style={{ color: "red", paddingLeft: 10, fontSize: 15 }}>
+                    비밀번호가 일치하지 않습니다.
+                  </div>
+                )}
+              </div>
+              <TextBox
+                type="password"
+                style={{ fontSize: 35 }}
+                value={checkPassword}
+                onChange={(e) => setCheckPassword(e.target.value)}
+              />
+            </InfoWrapper>
+            <LoginButton className="heading4" onClick={handleSubmit}>
+              회원가입
+            </LoginButton>
+          </LoginBox>
+        </div>
+      </Container>
+    </>
+  );
+};
 
 const Container = styled.div`
   display: flex;
@@ -19,8 +105,8 @@ const HomeLogo = styled.img`
 // 주황색 박스
 const LoginBox = styled.div`
   border-radius: 100px;
-  width: 765px;
-  height: 906px;
+  width: 700px;
+  height: 800px;
   background-color: rgba(255, 160, 122, 0.3);
   display: flex;
   flex-direction: column;
@@ -36,38 +122,28 @@ const LoginButton = styled.button`
   width: 150;
   display: flex;
   justify-content: center;
-  color: #2f2f2f;
+  color: #ffffff;
   box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
   width: 154px;
   border-width: 0;
   cursor: pointer;
 `;
 
-export const SignUp = () => {
-  const navigate = useNavigate();
-  return (
-    <>
-      <Container>
-        <HomeLogo src={logo} alt="linkrap image" />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <LoginBox>
-            회원가입 페이지
-            <LoginButton
-              className="heading3"
-              onClick={() => navigate("/setnickname")}
-            >
-              Join
-            </LoginButton>
-          </LoginBox>
-        </div>
-      </Container>
-    </>
-  );
-};
+// 텍스트 + 텍스트박스
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+`;
+
+// 아이디, 비밀번호 받는 텍스트 박스
+const TextBox = styled.input`
+  width: 515px;
+  height: 59px;
+  border-radius: 30px;
+  background-color: rgba(255, 255, 255, 0.7);
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  border-width: 0;
+  font-size: 20px;
+  padding-left: 15px;
+`;
