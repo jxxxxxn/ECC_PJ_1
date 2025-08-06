44106179662e1ef021eae045383fb24888f90e74
bhyerain <seunghye0307@naver.com>
2025-08-07 01:57:21 +0900

    [Chore] index 수정

--- FE/src/pages/index.js
+++ FE/src/pages/index.js
@@ -2,4 +2,5 @@ export { Home } from "./Home";
 export { FriendList } from "./FriendList";
 export { LinkUpload } from "./LinkUpload";
 export { Post } from "./Post";
+export { PostEdit } from "./PostEdit";
 export * from "./mypage";
