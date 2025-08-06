98d4aa882b48fa17dce736eb5260cfeedc682ac8
bhyerain <seunghye0307@naver.com>
2025-08-07 02:02:49 +0900

    [Style] padding 추가

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -3,11 +3,19 @@ import PostCategory from "../components/PostCategory";
 import { PageHeader } from "../components/PageHeader";
 import { useState } from "react";
 
-
 export const Home = () => {
   const [activeTab, setActiveTab] = useState("all");
   return (
-    <div style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column", gap: "15px" }}>
+    <div
+      style={{
+        padding: 10,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "15px",
+      }}
+    >
       <PageHeader title="링크집" />
       <PostCategory activeTab={activeTab} onTabClick={setActiveTab} />
       <PostlistLayout />
