6bd0d8674b22a859c95cf0ad0a3a05f1cd8449e3
offonof <chaneun0424@gmail.com>
2025-08-08 14:50:09 +0900

    [Feat] 회원 정보 수정 페이지 구현

--- FE/src/pages/mypage/ProfileCard.jsx
+++ FE/src/pages/mypage/ProfileCard.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useParams, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import profile from "../../assets/profile.jpg";
 
 
