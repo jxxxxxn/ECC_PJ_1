4a3f83d31eb0ddb2f493e0985de760dbb9133874
bhyerain <seunghye0307@naver.com>
2025-08-24 03:11:02 +0900

    [Style] 스타일 수정

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -22,13 +22,12 @@ const PageButton = styled.div`
 
 const PageNumber = styled.div`
   color: black;
-  font-size: 20px;
+  font-size: 18px;
   font-family: "Pretendard";
-  font-weight: 400;
+  font-weight: ${({ $active }) => ($active ? 600 : 400)};
   text-align: center;
   line-height: 40px;
   user-select: none;
-
   text-decoration: ${({ $active }) => ($active ? "underline" : "none")};
   text-underline-offset: 8px;
   text-decoration-thickness: 1.5px;
@@ -38,7 +37,7 @@ const NextButton = styled.div`
   width: 57px;
   height: 40px;
   color: black;
-  font-size: 20px;
+  font-size: 18px;
   font-family: "Pretendard";
   font-weight: 400;
   cursor: pointer;
@@ -56,10 +55,7 @@ const Pagination = ({ totalPages = 5 }) => {
       <PageList>
         {pages.map((page) => (
           <PageButton key={page} onClick={() => setCurrentPage(page)}>
-            <PageNumber
-              page={page}
-              $active={page === currentPage}
-            >
+            <PageNumber page={page} $active={page === currentPage}>
               {page}
             </PageNumber>
           </PageButton>
