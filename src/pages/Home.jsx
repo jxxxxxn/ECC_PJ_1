6c0acc049524ae1527c39c407171cf3ecbe0dfea
bhyerain <seunghye0307@naver.com>
2025-08-25 04:26:00 +0900

    [Style] 링마인드 위치 변동

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,34 @@
-import { PostlistLayout } from './postlist';
+import { PostlistLayout } from "./postlist";
 import { PageHeader } from "../components/PageHeader";
 import LinkMind from "../components/LinkMind";
 
 export const Home = () => {
   return (
-    <div style={{ padding: 10, flex: 3, display: "flex", flexDirection: "column", gap: "15px", }}>
+    <div
+      style={{
+        padding: 10,
+        flex: 3,
+        display: "flex",
+        flexDirection: "column",
+        gap: "15px",
+      }}
+    >
       <PageHeader title="링크집" />
-      <div style={{ flex: 3, display: "flex", flexDirection: "row", gap: "35px", }}>
-        <div style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column", gap: "15px", }}>  
+      <div
+        style={{ flex: 3, display: "flex", flexDirection: "row", gap: "35px" }}
+      >
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "15px",
+          }}
+        >
           <PostlistLayout />
         </div>
-        <div style={{ flex: 1, marginTop: "30px", }}> 
+        <div style={{ flex: 1 }}>
           <LinkMind />
         </div>
       </div>
